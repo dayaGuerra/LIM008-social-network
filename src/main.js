4c1b48a8af0c3774f059bd5d730d0049258ba0df
dayaGuerra <dayamandalaman@gmail.com>
2019-01-24 18:38:26 -0500

    Evento ocultar y mostrar pantalla Login

--- src/main.js
+++ src/main.js
@@ -3,3 +3,10 @@
 import { myFunction } from './lib/index.js';
 
 myFunction();
+
+const loginBtn = document.getElementById('login-btn-access');
+
+loginBtn.addEventListener('click', () => {
+    document.getElementById('section-intro-network').style.display = 'none';
+    document.getElementById('section-intro-network-login').style.display = 'block';
+});
