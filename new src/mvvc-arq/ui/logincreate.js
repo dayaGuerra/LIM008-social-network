57b942e971002534edf7153c5c5195e07120d4b8
dayaGuerra <dayamandalaman@gmail.com>
2019-01-31 15:00:01 -0500

    Arquitectura Proyecto

--- new src/mvvc-arq/ui/logincreate.js
+++ new src/mvvc-arq/ui/logincreate.js
@@ -4,12 +4,39 @@ export const signInForm = () => {
   const tempformLogin = 
   // Segundo, creas el nodo hijo o template que va a ir en ese elemento
   `
-    <p> hola munddsdsdsssssssssssssssssssssssssso!!! <p>
+<div id="signin">
+  <div class="text-first text-login">
+    <h4>La primera Red Social para emprendedores peruanos</h4>
+  </div>
+    
+      <div class="col-form-login-account">
+        <p class="text-login text-first-sesion">Iniciar Sesión</p>
+        <button class="fb btn-social-media" id = "login-faceboo">
+          <i class="fa fa-facebook fa-fw space-media"></i> Facebook
+        </button>   
+        <button class="google btn-social-media"  id = "login-google"><i class="fa fa-google fa-fw space-media">
+          </i> Google+  
+        </button>
+        <div class="space-form"><span>o</span></div>
+      </div>
+    <div id = "login-or-signup">
+    <div class="register-inputs-field col-form-create-account">
+    <input type="text" class="login-social-media" id="name-social-media" placeholder="Nombre">
+    <input type="text" class="login-social-media" id="lastname-social-media" placeholder="Apellido">
+    <select name="" id="" class="login-social-media">
+    <option value="">Cuentanos a que te dedicas</option>
+    <option value=""></option>
+    </select>
+    <input type="email" class="login-social-media" id="create-email" placeholder="Correo electronico">
+    <input type="password" class="login-social-media" id="create-password" placeholder="Contraseña">
+    </div>
+    <button class="login-social-media btn-login" id="btn-signup">Registrate</button>
+    </div>
+</div>
       `; 
+
   const formElem = document.createElement('form');
   formElem.setAttribute('id', 'form-login');
   formElem.innerHTML = tempformLogin;
   return formElem;
 };
-
-
