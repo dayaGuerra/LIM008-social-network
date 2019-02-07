c29bad46694dc922c10611171939676024e00109
JanetGM <antaly.146@gmail.com>
2019-02-07 16:21:00 -0500

    agregando test de deletePost

--- test/post.spec.js
+++ test/post.spec.js
@@ -17,24 +17,39 @@ const fixtureData = {
       }
     }
   }
-}
+};
 
 
-global.firebase = new MockFirebase(fixtureData, {isNaiveSnapshotListenerEnabled : true});
+global.firebase = new MockFirebase(fixtureData, {isNaiveSnapshotListenerEnabled: true});
 
-import {addNewPost, getAllPost} from '../new src/mvvc-arq/lib/controller/controller-post.js';
+import {addNewPost, getAllPost, deletePost} from '../new src/mvvc-arq/lib/controller/controller-post.js';
 
 
 describe('addNewPost', () => {
   it('debería de poder agregar una nota', () => {
     return addNewPost('adventure of the lifetime beibis').then((data) => {
-        const callback = (post) => {
-            
+      const callback = (post) => {
         console.log(post);
-        };
+      };
         // función de callback recibe la data
-        getAllPost(callback)
-        // recibe como argumento una función
+      getAllPost(callback);
+      // recibe como argumento una función
     });
+  });
+});
+
+describe('deletePost', () => {
+  it('debería de poder eliminar una nota', () => {
+    return deletePost('abc123')
+      .then(() => {
+        const callback = (post) => {
+          const result = notes.find((elemento) => {
+            return elemento.id === 'abc123';
+          });
+          expect(result).toBe(undefined);
+          done();
+        };
+        getAllPost(callback);
+      });
   });
 });
